5060e3ba87e0c62fb75cc8c806ecfa26c71eaadb
pooja12334 <104409514+pooja12334@users.noreply.github.com>
2022-05-01 16:33:22 +0530

    Add files via upload

---
+++ JSfile1.js
@@ -0,0 +1,84 @@
+/*Varibable Declaration
+1. var 
+2. let
+3. const*/
+
+/*
+var b = "R"
+var c = "A"
+console.log(b);
+console.log(c)
+*/
+
+/*
+let a = "z"
+let b = "y"
+console.log(a);
+console.log(b);
+*/
+
+/*
+const q = "abc"
+const p = "xyz"
+console.log(q);
+console.log(p);
+*/
+
+//OPERATOR:- =, ==, ===
+
+/*
+let b ="12"
+let a = 12
+let c = a==b;
+let d = a===b;
+console.log(c);
+console.log(d);*/
+ 
+// if else condition
+/*
+if(true)
+{
+    console.log("abc");
+}
+else
+{
+    console.log("xyz")
+}
+*/
+
+// TERNARY OPERATOR: NESTED
+
+/*
+let a = true
+a ? b = true : b = false ? c = 'b' : c = 'xyz'
+//console.log(b);
+console.log(c);
+*/
+
+let c = true
+let a = "abc"
+let b = "xyz"
+ d= c ? true:false ? a : b
+console.log(d);
+
+
+// TYPEOF:-
+
+ /*a = null
+ //typeof(a)
+ console.log(typeof(a));*/
+
+
+/*a = 0
+console.log(typeof(a));*/
+
+/*a = []
+console.log(typeof(a));*/
+
+/*let d 
+console.log(d)*/
+
+
+
+
+
